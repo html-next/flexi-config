f36e4b063ec517ce085de2ccd83af34514cf022e
Kyle Bishop <kybishop@gmail.com>
2017-03-15 13:27:11 -0700

    more eslint fixes

--- tests/dummy/app/router.js
+++ tests/dummy/app/router.js
@@ -1,12 +1,20 @@
-import Ember from 'ember';
+import EmberRouter from 'ember-router';
 import config from './config/environment';
 
-const Router = Ember.Router.extend({
+const Router = EmberRouter.extend({
   location: config.locationType,
   rootURL: config.rootURL
 });
 
 Router.map(function() {
+  this.route('site-index');
+
+  this.route('tests', function() {
+    this.route('layouts');
+    this.route('mobile-first');
+  });
+
+  this.route('classic-layout-test');
 });
 
 export default Router;
